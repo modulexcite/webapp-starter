4d05b846d00c96bffa914be9911b31e6d6ce6d75
Jon Dugan <jdugan@x1024.net>
2015-09-25 16:30:04 -0500

    linting

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
-router.get('/', function(req, res) {
-	res.sendfile('./public/index.html');
+router.get("/", function (req, res) {
+    res.sendfile("./public/index.html");
 });
 
 module.exports = router;
